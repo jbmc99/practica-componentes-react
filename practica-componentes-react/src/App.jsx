8123dc57bd98a7ad69256abd381d3351eb084a05
jbmc99 <127203600+jbmc99@users.noreply.github.com>
2024-07-04 14:10:12 +0200

    detect winner

--- practica-componentes-react/src/App.jsx
+++ practica-componentes-react/src/App.jsx
@@ -4,19 +4,90 @@ const TURNS = {
   O: "o",
 };
 
-const Square = ({ children, updateBoard, index }) => {
-  return <div className="square">{children}</div>;
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+  const handleClick = () => {
+    updateBoard(index);
+  };
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
+
+const WINNER_COMBOS = [
+  [0, 1, 2], //horizontal
+  [3, 4, 5], //horizontal
+  [6, 7, 8], //horizontal
+  [0, 3, 6], //vertical
+  [1, 4, 7], //vertical
+  [2, 5, 8], //vertical
+  [0, 4, 8], //diagonal
+  [2, 4, 6], //diagonal
+];
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null); //null es que no hay ganador,
+  //false es que hay un empate y el valor de la constante TURNS es el ganador
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras
+    // para ver si X o O ganó
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] && // 0 => x u o
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
+
+  const updateBoard = (index) => {
+    //no actualizamos esta posición si ya tiene un valor
+    if (board[index] || winner) return;
+
+    //actualizar el tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    //cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    //revisar si hay un ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(() => 
+    
+    );
+      // alert(`El ganador es ${newWinner}`); //salta 
+      //la alerta antes de poner el ganador en el tablero porque
+      // el estado se actualiza de manera asincrona
+    }
+  };
+
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
         {board.map((_, index) => {
-          return <Square key={index} index={index}></Square>;
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
         })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   );
